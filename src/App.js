bdd62802e45e10c518bb773e8b916836bf730815
Fabian Cerchi <angelfcerchi@gmail.com>
2023-06-23 23:40:37 -0300

    Feat: Se agregan estilos a las cards de los productos

--- src/App.js
+++ src/App.js
@@ -4,46 +4,64 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Producto from './components/Producto';
 import Carrito from './components/Carrito';
+import { Container, Col, Row } from 'react-bootstrap';
 
 function App() {
   const carritoGuardado = JSON.parse(localStorage.getItem("carrito")) || [];
 
   const [carrito, agregarProducto] = useState(carritoGuardado);
-  
+
   useEffect(() => {
     localStorage.setItem("carrito", JSON.stringify(carrito));
   }, [carrito]);
 
   // Creamos nuestra lista de productos
   const [productos] = useState([
-    {id:0, articulo:"Pure de Tomate", precio:100},
-    {id:1, articulo:"Mayonesa", precio:200},
-    {id:2, articulo:"Pringles", precio:800},
-    {id:3, articulo:"Oreo", precio:300}
+    { id: 0, nombre: "Arroz", precio: 80, img: "https://example.com/imagen-arroz.jpg" },
+    { id: 1, nombre: "Fideos", precio: 60, img: "https://example.com/imagen-fideos.jpg" },
+    { id: 2, nombre: "Aceite de Oliva", precio: 200, img: "https://example.com/imagen-aceite.jpg" },
+    { id: 3, nombre: "Leche", precio: 120, img: "https://example.com/imagen-leche.jpg" },
+    { id: 4, nombre: "Pan", precio: 30, img: "https://example.com/imagen-pan.jpg" },
+    { id: 5, nombre: "Jabón de Lavandería", precio: 50, img: "https://example.com/imagen-jabon.jpg" },
+    { id: 6, nombre: "Detergente para Platos", precio: 70, img: "https://example.com/imagen-detergente.jpg" },
+    { id: 7, nombre: "Papel Higiénico", precio: 90, img: "https://example.com/imagen-papel.jpg" },
+    { id: 8, nombre: "Café", precio: 150, img: "https://example.com/imagen-cafe.jpg" },
+    { id: 9, nombre: "Azúcar", precio: 40, img: "https://example.com/imagen-azucar.jpg" },
+    { id: 10, nombre: "Cepillo de Dientes", precio: 150, img: "https://example.com/imagen-cafe.jpg" },
+    { id: 11, nombre: "Desodorante", precio: 30, img: "https://example.com/imagen-azucar.jpg" }
   ])
 
   return (
     <>
       <Header
-          titulo="El Mercadito"
+        titulo="El Mercadito"
       />
-      <h1>Listado de productos</h1>
-      {
-        productos.map(producto => 
-        <Producto
-            producto={producto}
-            productos={productos}
-            carrito = {carrito}
-            agregarProducto = {agregarProducto}
-            key={producto.id}
-        />)
-      }
-        <Carrito
-          carrito= {carrito}
-          agregarProducto = {agregarProducto}
-        />
+      <h1 className='text-center'>Listado de productos</h1>
+      <Container className=''>
+        <Row>
+          {
+            productos.map(producto =>
+              <>
+                <Col>
+                  <Producto
+                    producto={producto}
+                    productos={productos}
+                    carrito={carrito}
+                    agregarProducto={agregarProducto}
+                    key={producto.id}
+                  />
+                </Col>
+              </>
+            )
+          }
+        </Row>
+      </Container>
+      <Carrito
+        carrito={carrito}
+        agregarProducto={agregarProducto}
+      />
 
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Card, Button} from 'react-bootstrap';
 
 const Producto = ({ producto, productos, carrito, agregarProducto }) => {
 
     //Extraigo los valores de producto
-    const { id, articulo, precio, cantidadComprada } = producto; //Destructuring
+    const { id, nombre, precio, cantidadComprada } = producto; //Destructuring
 
     //Funcion para comprar un producto
     const seleccionarProducto = (id) => {
@@ -11,12 +12,12 @@ const Producto = ({ producto, productos, carrito, agregarProducto }) => {
             producto => producto.id === id
         )[0];
         const productoCarrito = carrito.find(producto => producto.id === id);
-        if(!productoCarrito){
+        if (!productoCarrito) {
             producto.cantidadComprada = 1
             agregarProducto([...carrito, producto]);
-        }else{
-           productoCarrito.cantidadComprada += 1
-           agregarProducto([...carrito])
+        } else {
+            productoCarrito.cantidadComprada += 1
+            agregarProducto([...carrito])
         }
     };
 
@@ -24,10 +25,10 @@ const Producto = ({ producto, productos, carrito, agregarProducto }) => {
         const producto = carrito.filter(
             producto => producto.id === id
         )[0];
-        if(producto.cantidadComprada > 1){
-            producto.cantidadComprada-=1
+        if (producto.cantidadComprada > 1) {
+            producto.cantidadComprada -= 1
             agregarProducto([...carrito])
-        }else{
+        } else {
             const carritoActualizado = carrito.filter(producto => producto.id !== id);
             agregarProducto(carritoActualizado)
         }
@@ -38,15 +39,21 @@ const Producto = ({ producto, productos, carrito, agregarProducto }) => {
 
             {productos ?
                 <>
-                    <li>{id} | {articulo} | {precio}</li>
-                    <button
-                        type="button"
-                        onClick={() => seleccionarProducto(id)}
-                    >Comprar</button>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={ nombre } />
+                        <Card.Body>
+                            <Card.Title>{ nombre }</Card.Title>
+                            <Card.Text>{ nombre }</Card.Text>
+                            <Card.Text>Precio: { precio }</Card.Text>
+                            <Button variant="primary"
+                                type="button"
+                                onClick={() => seleccionarProducto(id)}>Agregar al carrito</Button>
+                        </Card.Body>
+                    </Card>
                 </>
                 :
-                <>  
-                    <li>{id} | {articulo} | {precio} | {cantidadComprada}</li>
+                <>
+                    <li>{id} | {nombre} | {precio} | {cantidadComprada}</li>
                     <button
                         type="button"
                         onClick={() => eliminarProducto(carrito, id)}
