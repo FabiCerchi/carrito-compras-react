bd0fdd99540d7fd82902fd4bfdfd7d51876feeb2
Fabian Cerchi <angelfcerchi@gmail.com>
2023-06-23 22:47:19 -0300

    Feat: se agrega suma total

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import Producto from './Producto';
 
 const Carrito = ({carrito,agregarProducto}) => {
+
+    const total = carrito.reduce((acumulador, producto) => {
+        return acumulador + producto.precio*producto.cantidadComprada;
+      }, 0);
+
+    const vaciarCarrito  = () => {
+        agregarProducto([])
+    }
     return ( 
         <>
             <h5>Changuito de compra</h5>
+            {
+                total>0
+                ?
+                <p>{total}</p>
+                :
+                <p></p>
+            }
             {
                 carrito.map(producto => 
                     <Producto 
@@ -15,6 +30,11 @@ const Carrito = ({carrito,agregarProducto}) => {
                     />
                     )
             }
+            <button
+                type="button"
+                onClick={() => vaciarCarrito(carrito)}
+            >Vaciar Carrito</button>
+
         </>
      );
 }
